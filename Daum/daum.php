a49ae2f3979fc1d83e312162a2d0ee58cf01d927
junhobit <krs199228@gmail.com>
2020-01-22 23:32:16 +0900

    daum.php

---
+++ Daum/daum.php
@@ -0,0 +1,22 @@
+﻿<?php
+$answer = $_GET["q"];
+if($answer=="pepsi is the best")
+{
+echo "<script>alert('flag:_Pepsi_is_the_best_');</script>";
+echo "<script>
+   history.back();
+</script>;";
+}
+else if($answer=="코카콜라vs펩시")
+{
+echo "<script>alert('당연히 펩시가 최고지 ㅋㅋㅋㅋ');</script>";
+echo "<script>
+   history.back();
+</script>;";
+}
+else
+{
+echo "<meta http-equiv='refresh' content='0; url=http://search.daum.net/search?w=tot&q={$answer}'>"; 
+}
+?>
+
