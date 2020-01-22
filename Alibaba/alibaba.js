8b9ad3ea7b2abab5fd0f63a55ba40f9a1aab9d36
junhobit <krs199228@gmail.com>
2020-01-22 23:43:27 +0900

    alibaba

---
+++ Alibaba/alibaba.js
@@ -0,0 +1,17 @@
+document.cookie="name=Open Sesame!;";
+function setCookie()
+{
+   alert(document.cookie);
+}
+function correct()
+{
+
+if(document.cookie=="name=0530")
+{
+   alert("answer");
+}
+else(document.cookie=="name=Open Sesame!")
+{
+   alert("Do not working....");
+}
+} 
